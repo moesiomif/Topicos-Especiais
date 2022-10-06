749d11e08642692e855abffaa7620609c3a4424c
Danilo Ferreira <49424200+danilosheen@users.noreply.github.com>
2022-10-06 16:35:18 -0300

    Update q2-mdc-euclides.py

--- Tema-I/q2-mdc-euclides.py
+++ Tema-I/q2-mdc-euclides.py
@@ -7,3 +7,19 @@
 # Autor: Moésio M. de Sales
 # site:  github.com/moesiomif/Topicos-Especiais
 
+def mdc_euclides(a, b):
+
+    anterior  = a
+    atual     = b
+
+    resto = atual % anterior
+    while resto != 0:
+        resto = anterior % atual
+        anterior = atual
+        atual = resto
+
+    return anterior
+
+print("Informe o valor de A e B: ", end='')
+a, b = map(int, input().split())
+print("MDC de {} e {} = {}".format(a, b, mdc_euclides(a, b)))
